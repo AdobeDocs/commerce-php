63b0a01d6ede239756bc2e012dac9b83d0694e59
Jeff Matthews <matthews.jeffery@gmail.com>
2022-05-13 15:01:07 -0500

    Added TOC structure

--- src/data/navigation/sections/architecture.js
+++ src/data/navigation/sections/architecture.js
@@ -3,4 +3,84 @@ module.exports = [
     title: "Introduction",
     path: "/architecture/",
   },
+  {
+    title: "Architectural basics",
+    path: "/architecture/basics/",
+    pages: [
+      {
+        title: "Diagrams",
+        path: "/architecture/basics/diagrams",
+      },
+      {
+        title: "Frontend customization",
+        path: "/architecture/basics/frontend-customization/",
+      },
+      {
+        title: "Security",
+        path: "/architecture/basics/security",
+      },
+    ],
+  },
+  {
+    title: "Architectural layers",
+    path: "/architecture/layers/",
+    pages: [
+      {
+        title: "Domain layer",
+        path: "/architecture/layers/domain/",
+      },
+      {
+        title: "Persistence layer",
+        path: "/architecture/layers/persistence/",
+      },
+      {
+        title: "Presentation layer",
+        path: "/architecture/layers/presentation/",
+      },
+      {
+        title: "Service layer",
+        path: "/architecture/layers/service/",
+      },
+    ],
+  },
+  {
+    title: "Coding standards",
+    path: "https://devdocs.magento.com/guides/v2.4/coding-standards/bk-coding-standards.html",
+  },
+  {
+    title: "Framework",
+    path: "/architecture/framework/",
+  },
+  {
+    title: "Modules and extensibility",
+    path: "/architecture/modules/",
+    pages: [
+      {
+        title: "Module overview",
+        path: "/architecture/modules/overview/",
+      },
+      {
+        title: "Modules and areas",
+        path: "/architecture/modules/areas/",
+      },
+      {
+        title: "Module relationships",
+        path: "/architecture/modules/relationships/",
+      },
+      {
+        title: "Module dependencies",
+        path: "/architecture/modules/dependencies/",
+      },
+    ],
+  },
+  {
+    title: "Technical vision",
+    path: "/architecture/technical-vision/",
+    pages: [
+      {
+        title: "Web API",
+        path: "https://devdocs.magento.com/guides/v2.4/get-started/bk-get-started-api.html"
+      }
+    ],
+  },
 ];
