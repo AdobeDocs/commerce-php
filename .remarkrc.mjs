1062ec0da0749523a833a3e0087931a27e6c2952
Dima Shevtsov <shevtsov@adobe.com>
2024-06-20 13:41:22 -0500

    Delete duplication

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -31,7 +31,6 @@ const remarkConfig = {
 					"https://eat.magento.com",
 					"https://developer.ups.com/oauth-developer-guide",
 					"https://business.adobe.com/products/magento/business-intelligence.html",
-					"https://business.adobe.com/products/magento/business-intelligence.html",
 					"https://www.adobe.com/trust/security/product-security.html"
 				],
 				skipOffline: "true"
