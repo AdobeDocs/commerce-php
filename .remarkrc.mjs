01f8a06ab2494a2315062af1eda4d46b50d989ed
Dima Shevtsov <shevtsov@adobe.com>
2024-07-23 14:57:23 -0500

    Revert skipUrlPatterns

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -25,14 +25,14 @@ const remarkConfig = {
 			remarkLintNoDeadUrls,
 			{
 				skipUrlPatterns: [
-					"https://www.php.net",
-					"https://cardinalcommerce.com/",
-					"https://www.cyberciti.biz",
-					"https://eat.magento.com",
-					"https://developer.ups.com/oauth-developer-guide",
-					"https://business.adobe.com/products/magento/business-intelligence.html",
-					"https://business.adobe.com/products/magento/business-intelligence.html",
-					"https://www.adobe.com/trust/security/product-security.html"
+					// "https://www.php.net",
+					// "https://cardinalcommerce.com/",
+					// "https://www.cyberciti.biz",
+					// "https://eat.magento.com",
+					// "https://developer.ups.com/oauth-developer-guide",
+					// "https://business.adobe.com/products/magento/business-intelligence.html",
+					// "https://business.adobe.com/products/magento/business-intelligence.html",
+					// "https://www.adobe.com/trust/security/product-security.html"
 				],
 				skipOffline: "true"
 			}
