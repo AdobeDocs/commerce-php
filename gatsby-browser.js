0055bf398bb066e9018c2656ef250fc44e00bcf6
Dima Shevtsov <12731225+dshevtsov@users.noreply.github.com>
2025-05-14 17:00:00 -0500

    Delete redundant header

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -24,12 +24,6 @@ const components = {
 export const wrapRootElement = ({ element }) => {
   return <MDXProvider components={components}>{element}</MDXProvider>;
 };
-/*
- *  Copyright 2025 Adobe All Rights Reserved.
- *  NOTICE:  All information contained herein is, and remains the property of Adobe and its suppliers, if any.
- *  The intellectual and technical concepts contained herein are proprietary to Adobe and its suppliers and are protected by all applicable intellectual property laws, including trade secret and copyright laws.
- *  Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Adobe.
- */
 
 const isBrowser = typeof window !== "undefined";
 
